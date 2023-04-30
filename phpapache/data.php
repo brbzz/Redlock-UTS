6fc071f93b87cd80125a4fc561f8b816cebba7b1
brbzz <bryanrafferty04@gmail.com>
2023-04-30 14:07:51 -0400

    UTS SNA 2

--- phpapache/data.php
+++ phpapache/data.php
@@ -16,7 +16,7 @@ $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
 	$row = mysqli_fetch_assoc($result);
-	echo "<Total Data> " . $row["TOTAL"];
+	$row["TOTAL"];
 }
 
 else {
