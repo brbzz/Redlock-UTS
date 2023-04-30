7019b1041e9727d36b8dd190f93c0a9088af26b9
brbzz <bryanrafferty04@gmail.com>
2023-04-30 13:55:15 -0400

    UTS SNA 2

---
+++ phpapache/data.php
@@ -0,0 +1,28 @@
+<?php
+// Connect to the database
+$servername = "172.18.0.4";
+$username = "root";
+$password = "inipasswordsaya";
+$dbname = "Redlock";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+	die("Connection Failed: " . mysqli_connect_error());
+}
+
+// Display all users
+$sql = "SELECT COUNT(*) AS TOTAL FROM users";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+	$row = mysqli_fetch_assoc($result)
+	echo "<Total Data> " . $row["TOTAL"];
+}
+
+else {
+	echo "No users found.";
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
